37b666fef03e31ceed98f349d0d9c487ea3d25a0
unknown <amiravni83@gmail.com>
2014-10-31 00:04:40 +0200

    All encoder test

--- ScrobotInt.ino
+++ ScrobotInt.ino
@@ -10,44 +10,29 @@ const int MS1 = 2;
 
 // —————————————————————————  Encoders
 
-const int pin_1_P0 = 22;  // Encoder input pins
-const int pin_1_P1 = 23;
-int FW1 = 0;
-volatile long  counter1=0;
+const int pin_P0[6] = {22,24,26,28,30,32};  // Encoder input pins
+const int pin_P1[6] = {23,25,27,29,31,33};
+int FW[6] = {0,0,0,0,0,0};
+volatile long  counter[6]={0,0,0,0,0,0};
 
 // —————————————————————————  Motors
-const int motor1[] = {   14, 15};
-const int motor2[] = {   4, 5};
-const int motor3[] = {   6, 7};
-const int motor4[] = {   8, 9};
-const int motor5[] = {   10, 11};
-const int motor6[] = {   12, 13};
-
+const int M1[6] = {14,4,6,8,10,12};
+const int M2[6] = {15,5,7,9,11,13};
 // ————————————————————————— Setup
 void setup() {
   Serial.begin(9600);
   encoder_begin();  // Start the library
-  attach_encoder(0, pin_1_P0, pin_1_P1);  // Attach an encoder to pins A and B
-  pinMode(pin_1_P0,INPUT_PULLUP);
-  pinMode(pin_1_P1,INPUT_PULLUP);
-  pinMode(MS1,INPUT);
-  // Setup motors
-  int i;
-  for(i = 0; i < 2; i++){
-    pinMode(motor1[i], OUTPUT);
-    pinMode(motor2[i], OUTPUT);   
-    pinMode(motor3[i], OUTPUT);
-    pinMode(motor4[i], OUTPUT);
-    pinMode(motor5[i], OUTPUT);
-    pinMode(motor6[i], OUTPUT);    
+  for (int i=0;i<6;i++) {
+    attach_encoder(i, pin_P0[i], pin_P1[i]);  // Attach an encoder to pins A and B
+    pinMode(pin_P0[i],INPUT_PULLUP);
+    pinMode(pin_P1[i],INPUT_PULLUP);
+    pinMode(M1[i],OUTPUT);
+    pinMode(M2[i],OUTPUT);
+    motorST(M1[i],M2[i]);
   }
+  pinMode(MS1,INPUT);
+
   pinMode(LEDPIN,OUTPUT);
-  motorST(motor1);
-  motorST(motor2);
-  motorST(motor3);
-  motorST(motor4);
-  motorST(motor5);
-  motorST(motor6);
   digitalWrite(LEDPIN,LOW);
 
 attachInterrupt(0, resetCounter, FALLING);
@@ -57,27 +42,33 @@ attachInterrupt(0, resetCounter, FALLING);
 
 void loop() {
   char cmd=0;
-  
+  int dir0[6]={0,0,0,0,0,0};
   unsigned long testTime = 500;
   if ( millis() - startTime  < testTime  ) { 
-
-  int dir_0 = encoder_data(0);  // First encoder
-  if(dir_0 != 0)  // Check for rotation
+    
+  for (int i=0;i<6;i++) {
+  dir0[i] = encoder_data(i);  // First 
+    if(dir0[i] != 0)  // Check for rotation
   {
-    counter1 += FW1*abs(dir_0);
- //   Serial.print("Encoder 0: ");
- //   Serial.println(dir_0);
+    counter[i] += FW[i]*abs(dir0[i]);
+  }
   }
   
   }
   else
   {
-  motorST(motor1);
-    FW1=0;
+      for (int i=0;i<6;i++) {
+    motorST(M1[i],M2[i]);
+    FW[i]=0;
+      }
   }
 
   if ( (millis() - startTime  > testTime + 50 ) && ( millis() - startTime< testTime + 55 ) ) { 
-     Serial.println(counter1);
+     for (int i=0;i<6-1;i++) {
+     Serial.print(counter[i]);
+     Serial.print("  ,  ");
+  }
+    Serial.println(counter[5]);
   }
 
   if (Serial.available() > 0) 
@@ -85,104 +76,40 @@ void loop() {
 
     cmd = Serial.read();
     if (cmd=='q') {
-          motorFW(motor1);
-          FW1 = 1;
+      for (int i=0;i<6;i++) {
+    motorFW(M1[i],M2[i]);
+    FW[i]=1;
+      }
     }
   else if (cmd == 'Q')  {
-          motorBW(motor1);
-        FW1 = -1;
+      for (int i=0;i<6;i++) {
+    motorBW(M1[i],M2[i]);
+    FW[i]=-1;
+      }
   }
  startTime = millis();
   }
 
-   /* switch (cmd) {
-
-    case 'q':
-      motorFW(motor1);
-      break;
-    case 'a':
-      motorBW(motor1);
-      break;   
-    case 'w':
-      motorFW(motor2);
-      motorFW(motor3);      
-      break;
-    case 's':
-      motorBW(motor2);
-      motorBW(motor3);     
-      break;     
-    case 'e':
-      motorFW(motor2);
-      motorBW(motor3);      
-      break;
-    case 'd':
-      motorBW(motor2);
-      motorFW(motor3);     
-      break;   
-    case 'r':
-      motorFW(motor4);
-      motorFW(motor5);      
-      break;
-    case 'f':
-      motorBW(motor4);
-      motorBW(motor5);     
-      break;     
-    case 't':
-      motorFW(motor4);
-      motorBW(motor5);      
-      break;
-    case 'g':
-      motorBW(motor4);
-      motorFW(motor5);     
-      break;      
-    case 'y':
-      motorFW(motor6);
-      break;
-    case 'h':
-      motorBW(motor6);
-      break;  
-      case 'u':
-      digitalWrite(LEDPIN,HIGH);
-      break;
-      case 'j':
-      digitalWrite(LEDPIN,LOW);
-      break;      
-    }
-    delay(DELAY);
-    motorST(motor1);
-    motorST(motor2);
-    motorST(motor3);
-    motorST(motor4);
-    motorST(motor5);
-    motorST(motor6);
-
-  }
-*/
-
- 
 }
 
 
 
-void motorFW(const int motor[2]){
-  digitalWrite(motor[0], HIGH);
-  digitalWrite(motor[1], LOW);
+void motorFW(const int mtr1, const int mtr2  ){
+  digitalWrite(mtr1, HIGH);
+  digitalWrite(mtr2, LOW);
 }
-void motorBW(const int motor[2]){
-  digitalWrite(motor[0], LOW);
-  digitalWrite(motor[1], HIGH);
+void motorBW(const int mtr1, const int mtr2  ){
+  digitalWrite(mtr2, HIGH);
+  digitalWrite(mtr1, LOW);
 }
-void motorST(const int motor[2]){
-  digitalWrite(motor[0], LOW);
-  digitalWrite(motor[1], LOW);
+void motorST(const int mtr1, const int mtr2  ){
+  digitalWrite(mtr1, LOW);
+  digitalWrite(mtr2, LOW);
 }
 
 void resetCounter()
 {
- // if (!digitalRead(MS1))
-//{
-counter1=0;
-//}
+counter[0]=0;
   
 }
 
